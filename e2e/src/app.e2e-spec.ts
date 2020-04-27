0961c549564c3c62455f4c8479dd880556f8de66
mtabbasi <mtabba30@gmail.com>
2020-04-27 08:46:38 +0430

    initial version

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('car-radar app is running!');
+    expect(page.getTitleText()).toEqual('Angular9 app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'car-radar'`, () => {
+  it(`should have as title 'Angular9'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('car-radar');
+    expect(app.title).toEqual('Angular9');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('car-radar app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Angular9 app is running!');
   });
 });
